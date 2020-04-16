872bbc7975ea7d5a1488e6a402b73d781f205823
Tim Godfrey <tim@webnerd.io>
2020-04-16 17:08:30 +0200

    Lesson 3: Components (Pt. 2)

--- src/App.vue
+++ src/App.vue
@@ -5,46 +5,31 @@
         Vuetify Dashboard
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn text rounded>Login</v-btn>
+      <v-btn
+        v-for="link in links"
+        :key="`${link.label}-header-link`"
+        :to="link.url"
+        text
+        rounded
+      >
+        {{ link.label }}
+      </v-btn>
     </v-app-bar>
     <v-content>
-      <!-- Login Component -->
-      <v-card width="400" class="mx-auto mt-5">
-        <v-card-title>
-          <h1 class="display-1">Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field label="Username" prepend-icon="mdi-account-circle" />
-            <v-text-field
-              label="Password"
-              :type="showPassword ? 'text' : 'password'"
-              prepend-icon="mdi-lock"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            />
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <router-view></router-view>
     </v-content>
     <v-footer color="primary lighten-1" padless>
       <v-row justify="center" no-gutters>
         <v-btn
           v-for="link in links"
-          :key="link"
+          :key="`${link.label}-footer-link`"
+          :to="link.url"
           color="white"
           text
           rounded
           class="my-2"
         >
-          {{ link }}
+          {{ link.label }}
         </v-btn>
         <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
           {{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
@@ -66,7 +51,20 @@ export default {
 
   data: () => ({
     showPassword: false,
-    links: ['Home', 'Login'],
+    links: [
+      {
+        url: '/',
+        label: 'Home',
+      },
+      {
+        url: '/login',
+        label: 'Login',
+      },
+      {
+        url: '/dashboard',
+        label: 'Dashboard',
+      },
+    ],
   }),
 };
 </script>
